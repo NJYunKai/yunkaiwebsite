555a0ec8d763606532220276c31ade9bd6f403f8
xiazhuyi <1640622502@qq.com>
2020-03-12 20:59:29 +0800

    一带一路

--- src/pages/beltroad/beltroad.vue
+++ src/pages/beltroad/beltroad.vue
@@ -1,7 +1,90 @@
 <template>
-    <div>
-        <h1>
-            一带一路
-        </h1>
+  <div class="r-news">
+    <div class="r-news-content">
+      <div class="content-title">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item v-for="(item,i) in breadList" :key="i">{{item}}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+
+      <div class="artdetail_title" style="display:block;font-family:Microsoft YaHei;font-size:24px;color:#000000;font-weight:bold;text-decoration:none;margin-left:395px;margin-bottom:20px">
+          一带一路 ———— 柬埔寨金边基地
+      </div>
+
+      <div class="artview_content" style="display:block;font-family:Microsoft YaHei;font-size:16px;color:#5A5A5A;font-weight:normal;text-decoration:none;">
+		<div class="artview_detail" setsid="article250">
+
+            <p>&nbsp; &nbsp;“一带一路”电商谷（金边）基地（简称“金边基地”）落地在柬埔寨工业技术学院，教学由电商谷南京分中心（江苏经贸职业技术学院）对口支撑，海外建设部分由南京云开数据科技有限公司投入。</p><br> &nbsp;<br> 
+
+            <p>&nbsp; &nbsp;金边基地于2017年12月16日达成合作，2019年4月26日建成并投入使用。金边基地根据柬埔寨的实际情况，第一阶段以信息技术应用普及为重点，成功举办了国家职业教育移动商务专业教学资源库在海外的应用推广活动。</p><br> &nbsp;<br> 
+
+            <p>&nbsp; &nbsp;2019年11月17日，金边基地相关建设方在金边召开了二期建设工作会议，确定了2020年工作规划，并与柬埔寨泰州商会组织的本地企业达成了开发本地电商平台的合作意向。</p><br> &nbsp;<br> 
+            
+            <div style="text-align: center;">
+                <img src="../img/cooperation.png" width="534" height="273" alt="">
+            </div> <br>
+            <p>&nbsp;&nbsp;2017年12月16日，江苏经贸职业技术学院与柬埔寨工业技术学院达成金边基地共建合作协议。中方薛茂云院长与柬方MOAN SAM OEURN院长共同签署了合作协议。</p><br> &nbsp;<br><br>
+            
+            <div style="text-align: center;">
+                <img src="../img/active.png" width="534" height="273" alt="">
+            </div> <br>
+             <p style="margin-left:150px">&nbsp;&nbsp;2019年4月26日，江苏经贸职业技术学院在金边基地举办国家职业教育移动商务专业教学资源库在海外的应用推广活动。</p><br> &nbsp;<br><br>
+
+            <div style="text-align: center;">
+                <img src="../img/meeting.png" width="546" height="298" alt="" style="padding-right: 0px;">
+            </div> <br>
+             <p style="margin-left:400px">&nbsp;&nbsp;2019年11月17日，金边基地召开二期建设工作会议。</p><br> &nbsp;<br><br>
+            
+		</div>
+		
+	</div>
     </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      isCollapse: false,
+      breadList: ["一带一路"]
+    };
+  },
+  //监听路由变化，更改菜单activeIndex值
+  watch: {
+    $route: "getPath"
+  },
+  created() {
+    this.getPath();
+  },
+  methods: {
+    //动态刷新面包屑
+    getPath() {
+      this.breadList = ["一带一路"];
+      switch (this.$route.path) {
+        //协同育人
+        case "/beltroad/beltroad":
+          this.breadList.push("一带一路");
+          break;
+      }
+      console.log(this.$route.path, this.breadList);
+    }
+  }
+};
+</script>
+
+<style>
+    .el-breadcrumb{
+        font-size: 20px;
+    }
+
+    p{
+        line-height: 2;
+    }
+
+    a{
+        color: red;
+    }
+
+</style>
