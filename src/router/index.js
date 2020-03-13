bcbd01fd54f86d0c44726e286f0fa42541ea8b4c
zhang-ch961 <zhang_ch961@qq.com>
2020-03-13 11:18:08 +0800

    删除冗余模块

--- src/router/index.js
+++ src/router/index.js
@@ -2,33 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import index from '@/pages/index';
 
-import about from '@/pages/about/about';
-import introduce from '@/pages/about/introduce';
-import honor from '@/pages/about/honor';
-import culture from '@/pages/about/culture';
-import partner from '@/pages/about/partner';
-
-import news from '@/pages/news/news';
-import newsIndex from '@/pages/news/newsIndex';
-import media from '@/pages/news/media';
-import topic from '@/pages/news/topic';
-import industry from '@/pages/news/industry';
-
-import financial from '@/pages/financial/financial';
-import credit from '@/pages/financial/credit';
-import iot from '@/pages/financial/iot';
-import banking from '@/pages/financial/banking';
-
-import government from '@/pages/government/government';
-import benevolent from '@/pages/government/benevolent';
-import supervise from '@/pages/government/supervise';
-import system from '@/pages/government/system';
-
-import offers from '@/pages/offers';
-import contact from '@/pages/contact';
-
-
-
 Vue.use(Router)
 
 export default new Router({
@@ -42,146 +15,6 @@ export default new Router({
         index: '1'
       },
       component: index
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: about,
-      meta: {
-        index: '2'
-      },
-      children: [{
-        path: 'introduce',
-        name: 'introduce',
-        meta: {
-          index: '2-1'
-        },
-        component: introduce,
-      }, {
-        path: 'honor',
-        name: 'honor',
-        meta: {
-          index: '2-2'
-        },
-        component: honor,
-      }, {
-        path: 'culture',
-        name: 'culture',
-        meta: {
-          index: '2-3'
-        },
-        component: culture,
-      }, {
-        path: 'partner',
-        name: 'partner',
-        meta: {
-          index: '2-4'
-        },
-        component: partner,
-      }]
-    },
-    {
-      path: '/news',
-      name: 'news',
-      meta: {
-        index: '3'
-      },
-      component: news,
-      children: [{
-        path: 'index',
-        name: 'index',
-        meta: {
-          index: '3-1'
-        },
-        component: newsIndex,
-      }, {
-        path: 'index/:id',
-        name: 'index',
-        meta: {
-          index: '3-1'
-        },
-        component: newsIndex,
-      }, {
-        path: 'media',
-        name: 'media',
-        meta: {
-          index: '3-2'
-        },
-        component: media,
-      }, {
-        path: 'topic',
-        name: 'topic',
-        meta: {
-          index: '3-3'
-        },
-        component: topic,
-      }, {
-        path: 'industry',
-        name: 'industry',
-        meta: {
-          index: '3-4'
-        },
-        component: industry,
-      }]
-    }, {
-      path: '/financial',
-      name: 'financial',
-      component: financial,
-      meta: {
-        index: '4'
-      },
-      children: [{
-        path: 'credit',
-        name: 'credit',
-        meta: {
-          index: '4-1'
-        },
-        component: credit,
-      }, {
-        path: 'iot',
-        name: 'iot',
-        meta: {
-          index: '4-2'
-        },
-        component: iot,
-      }, {
-        path: 'banking',
-        name: 'banking',
-        meta: {
-          index: '4-3'
-        },
-        component: banking,
-      }]
-    },
-    {
-      path: '/government',
-      name: 'government',
-      component: government,
-      meta: {
-        index: '5'
-      },
-      children: [{
-        path: 'benevolent',
-        name: 'benevolent',
-        meta: {
-          index: '5-1'
-        },
-        component: benevolent,
-      }, {
-        path: 'supervise',
-        name: 'supervise',
-        meta: {
-          index: '5-2'
-        },
-        component: supervise,
-      }, {
-        path: 'system',
-        name: 'system',
-        meta: {
-          index: '4-3'
-        },
-        component: system,
-      }]
     },
     //首页路由引用定义
     {
@@ -199,18 +32,18 @@ export default new Router({
       meta: {
         requireAuth: true
       },
-      component:() => import('../pages/productcase/productcase.vue'),
+      component: () => import('../pages/productcase/productcase.vue'),
       children: [{
-        path: '/productcase/xlab',
-        name: 'xlab',
-        component: () => import('../pages/productcase/xlab/xlab.vue')
-      },
+          path: '/productcase/xlab',
+          name: 'xlab',
+          component: () => import('../pages/productcase/xlab/xlab.vue')
+        },
         {
           path: '/productcase/gongshang',
           name: '/gongshang',
           component: () => import('../pages/productcase/gongshang/gongshang.vue')
         },
-    ]
+      ]
     },
     //协同育人路由引用定义
     {
@@ -246,22 +79,22 @@ export default new Router({
       meta: {
         requireAuth: true
       },
-      component: () =>import('../pages/companynews/companynews.vue'),
+      component: () => import('../pages/companynews/companynews.vue'),
       children: [{
-        path: '/companynews/dynamicnews',
-        name: 'dynamicnews' ,
-        component: () => import('../pages/companynews/dynamicnews/dynamicnews.vue')
-      },
-      {
-        path: '/companynews/fieldnews',
-        name: 'fieldnews' ,
-        component: () => import('../pages/companynews/fieldnews/fieldnews.vue')
-      },
-      {
-        path: '/companynews/1+x',
-        name: '1+x' ,
-        component: () => import('../pages/companynews/dynamicnews/1+x.vue')
-      }
+          path: '/companynews/dynamicnews',
+          name: 'dynamicnews',
+          component: () => import('../pages/companynews/dynamicnews/dynamicnews.vue')
+        },
+        {
+          path: '/companynews/fieldnews',
+          name: 'fieldnews',
+          component: () => import('../pages/companynews/fieldnews/fieldnews.vue')
+        },
+        {
+          path: '/companynews/1+x',
+          name: '1+x',
+          component: () => import('../pages/companynews/dynamicnews/1+x.vue')
+        }
       ]
     },
     //专业服务路由引用定义
@@ -308,6 +141,16 @@ export default new Router({
           component: () => import('../pages/services/labsolutions/labsolutions_coldchain.vue')
         },
         {
+          path: '/services/product/:type/:id',
+          name: 'product',
+          component: () => import('../pages/services/product/product.vue')
+        },
+        {
+          path: '/services/productdetails',
+          name: 'productdetails',
+          component: () => import('../pages/services/product/productdetails.vue')
+        },
+        {
           path: '/services/product_intelligence_manufacture',
           name: 'product_intelligence_manufacture',
           component: () => import('../pages/services/product/product_intelligence_manufacture.vue')
@@ -321,21 +164,21 @@ export default new Router({
           path: '/services/product_iot_teaching',
           name: 'product_iot_teaching',
           component: () => import('../pages/services/product/product_iot_teaching.vue')
-          },
-          {
-            path: '/services/product_iot_innovative',
-            name: 'product_iot_innovative',
-            component: () => import('../pages/services/product/product_iot_innovative.vue')
-            },
-		  {
-			path: '/services/teachingresource',
-			name: 'teachingresource',
-			component: () => import('../pages/services/teachingresource/teachingresource.vue')
-		  }
-		]
+        },
+        {
+          path: '/services/product_iot_innovative',
+          name: 'product_iot_innovative',
+          component: () => import('../pages/services/product/product_iot_innovative.vue')
+        },
+        {
+          path: '/services/teachingresource',
+          name: 'teachingresource',
+          component: () => import('../pages/services/teachingresource/teachingresource.vue')
+        }
+      ]
     },
-    	//专业建设路由引用定义
-	  {
+    //专业建设路由引用定义
+    {
       path: '/construction',
       name: 'construction',
       meta: {
@@ -343,62 +186,62 @@ export default new Router({
       },
       component: () => import('../pages/construction/construction.vue'),
       children: [{
-        path: '/construction/construction_iot',
-        name: 'construction_iot',
-        component: () => import('../pages/construction/computer/construction_iot.vue')
+          path: '/construction/construction_iot',
+          name: 'construction_iot',
+          component: () => import('../pages/construction/computer/construction_iot.vue')
         },
         {
-        path: '/construction/construction_bd',
-        name: 'construction_bd',
-        component: () => import('../pages/construction/computer/construction_bd.vue')
+          path: '/construction/construction_bd',
+          name: 'construction_bd',
+          component: () => import('../pages/construction/computer/construction_bd.vue')
         },
         {
-        path: '/construction/construction_cc',
-        name: 'construction_cc',
-        component: () => import('../pages/construction/computer/construction_cc.vue')
+          path: '/construction/construction_cc',
+          name: 'construction_cc',
+          component: () => import('../pages/construction/computer/construction_cc.vue')
         },
         {
-        path: '/construction/construction_ai',
-        name: 'construction_ai',
-        component: () => import('../pages/construction/computer/construction_ai.vue')
+          path: '/construction/construction_ai',
+          name: 'construction_ai',
+          component: () => import('../pages/construction/computer/construction_ai.vue')
         },
         {
-        path: '/construction/construction_ce',
-        name: 'construction_ce',
-        component: () => import('../pages/construction/computer/construction_ce.vue')
+          path: '/construction/construction_ce',
+          name: 'construction_ce',
+          component: () => import('../pages/construction/computer/construction_ce.vue')
         },
         {
-        path: '/construction/construction_bc',
-        name: 'construction_bc',
-        component: () => import('../pages/construction/computer/construction_bc.vue')
+          path: '/construction/construction_bc',
+          name: 'construction_bc',
+          component: () => import('../pages/construction/computer/construction_bc.vue')
         },
         {
-        path: '/construction/mechanical_ei',
-        name: 'mechanical_ei',
-        component: () => import('../pages/construction/mechanical/mechanical_ei.vue')
+          path: '/construction/mechanical_ei',
+          name: 'mechanical_ei',
+          component: () => import('../pages/construction/mechanical/mechanical_ei.vue')
         },
-       {
-        path: '/construction/mechanical_ir',
-        name: 'mechanical_ir',
-        component: () => import('../pages/construction/mechanical/mechanical_ir.vue')
-       },
-       {
-        path: '/construction/mechanical_ict',
-        name: 'mechanical_ict',
-        component: () => import('../pages/construction/mechanical/mechanical_ict.vue')
-       },
-       {
-        path: '/construction/other_de',
-        name: 'other_de',
-        component: () => import('../pages/construction/other/other_de.vue')
-       },
-       {
-        path: '/construction/electronic_de',
-        name: 'electronic_de',
-        component: () => import('../pages/construction/electronic/electronic_de.vue')
-       }
+        {
+          path: '/construction/mechanical_ir',
+          name: 'mechanical_ir',
+          component: () => import('../pages/construction/mechanical/mechanical_ir.vue')
+        },
+        {
+          path: '/construction/mechanical_ict',
+          name: 'mechanical_ict',
+          component: () => import('../pages/construction/mechanical/mechanical_ict.vue')
+        },
+        {
+          path: '/construction/other_de',
+          name: 'other_de',
+          component: () => import('../pages/construction/other/other_de.vue')
+        },
+        {
+          path: '/construction/electronic_de',
+          name: 'electronic_de',
+          component: () => import('../pages/construction/electronic/electronic_de.vue')
+        }
       ]
-      },
+    },
     {
       path: '/offers',
       name: 'offers',
@@ -406,14 +249,6 @@ export default new Router({
         index: '6'
       },
       component: offers
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      meta: {
-        index: '7'
-      },
-      component: contact
-    },
+    }
   ]
 })
