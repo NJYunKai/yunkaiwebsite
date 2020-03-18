6fd77945536677b6740855818c12a8ceb2579503
xiazhuyi <1640622502@qq.com>
2020-03-18 11:44:52 +0800

    协同育人

--- src/pages/cooperative-education/cooperative-education.vue
+++ src/pages/cooperative-education/cooperative-education.vue
@@ -9,7 +9,7 @@
       </div>
 
       <div class="news" style="margin: 0 15em;">
-        <div  class="artdetail_title" style="display:block;font-family:Microsoft YaHei;font-size:1.4em;color:#000000;font-weight:bold;text-decoration:none;margin: 1em 3em;text-align: center">
+        <div  class="artdetail_title" style="display:block;font-family:Microsoft YaHei;font-size:2em;color:#000000;font-weight:bold;text-decoration:none;margin: 1em 3em;text-align: center">
           <p>
             2019年第二批南京云开数据科技有限公司·教育部产学合作协同育人项目申报指南发布
           </p>
@@ -30,25 +30,63 @@
               <strong>申报指南（2019年第二批）的通知<br></strong></div>
             </strong>
 
-            <p style="text-indent:2em;">为深入贯彻全国教育大会精神，认真落实《国务院办公厅关于深化高等学校创新创业教育改革的实施意见》（国办发〔2015〕36号）和《国务院办公厅关于深化产教融合的若干意见》（国办发〔2017〕95号）要求，深化产教融合、产学合作、协同育人，经企业申报、产学合作协同育人项目专家组审议通过，形成了2019年第二批产学合作协同育人项目申报指南。本批次申报指南中，共有346家企业支持项目12350项，现予以公布（项目简介见附件）。</p><br> &nbsp;<br> 
+            <p style="text-indent:2em;">为了响应《国家教育事业“十三五”规划》关于推动产教融合的职业教育模式和深化本科教育教学改革的指导方针，贯彻教育部高教司关于产学合作协同育人的项目精神，南京云开数据科技有限公司面向全国高等院校提供新工科建设、教学内容和课程体系改革、师资培训、实践条件和实践基地建设、创新创业教育改革和创新创业联合基金项目支持。南京云开数据科技有限公司将持续为合作院校提供课程资源、师资培训、基础设施建设、实训实践基地建设、创新创业中心建设等服务，协助院校完成学科顶层设计，助力院校的课程体系建设、师资团队建设、教学改革，提升学生能力素质，以支持高校教学内容和课程体系改革与实践条件建设，促进高校实践教学创新改革，提升高校实验教学综合水平，推动高校复合型工程专业人才培养。</p><br> &nbsp;<br> 
                   
-            <div style="text-align: center;">
+            <!-- <div style="text-align: center;">
               <img src="../../../static/img/departmentinform.jpg" width="800" height="1053" alt="">
-            </div> <br>
-                  
-            <p style="text-indent:2em;">在教育部指导下，南京云开数据科技有限公司拟在“人工智能”、“物联网”、“嵌入式”、“微电子”、“云计算”、“电子通信”、“自动化”、“大数据”、“机器人”、“移动商务”等几个方向上，开展产学合作协同育人项目，支持包含新工科建设、教学内容和课程体系改革、师资培训、实践条件和实践基地建设、创新创业教育改革五大类。各大院校可在“教育部产学合作协同育人项目平台”（项目平台网址：<a href="http://cxhz.hep.com.cn">http://cxhz.hep.com.cn</a>）注册申报，欢迎各院校申报南京云开数据科技有限公司2019产学合作协同育人项目。</p><br> &nbsp;<br>
-                  
-            <div style="text-align: center;">
-              <img src="../../../static/img/companyedu2.png" width="800" height="283" alt="" style="padding-right: 0px;">
-            </div> <br>
-
-            <div style="text-align: center;">
-              <img src="../../../static/img/companyedu1.png" width="800" height="283" alt="" style="padding-right: 0px;">
-            </div> <br> <br> <br>
-                  
+            </div> <br> -->
+            
+            <!-- 项目简介 -->
             <div>
-              <p>点击查看协同育人申报指南链接：<a href="http://cxhz.hep.com.cn" style="color:red">http://cxhz.hep.com.cn</a></p>
-            </div>
+              <h1>一、项目简介</h1><br>
+
+              <p style="text-indent:2em;">在教育部指导下，南京云开数据科技有限公司拟在“人工智能”、“物联网”、“嵌入式”、“微电子”、“云计算”、“电子通信”、“自动化”、“大数据”、“机器人”、“移动商务”等几个方向上，开展产学合作协同育人项目，支持包含新工科建设、教学内容和课程体系改革、师资培训、实践条件和实践基地建设、创新创业教育改革五大类。各大院校可在“教育部产学合作协同育人项目平台”（项目平台网址：<a href="http://cxhz.hep.com.cn">http://cxhz.hep.com.cn</a>）注册申报，欢迎各院校申报南京云开数据科技有限公司2019产学合作协同育人项目。</p><br> &nbsp;<br>
+                    
+              <div style="text-align: center;">
+                <img src="../../../static/img/companyedu2.png" width="800" height="283" alt="" style="padding-right: 0px;">
+              </div> <br>
+
+              <div style="text-align: center;">
+                <img src="../../../static/img/companyedu1.png" width="800" height="283" alt="" style="padding-right: 0px;">
+              </div> <br> <br> <br>
+            </div><br>&nbsp;<br> 
+
+            <!-- 申报方法 -->
+            <div>
+              <h1>二、申请方法</h1><br>
+
+              <p style="text-indent:2em;">1、南京云开数据科技有限公司2019年第二批教育部产学合作协同育人包含五个项目：新工科建设、教学内容和课程体系改革、师资培训、实践条件和实践基地建设、创新创业教育改革项目，申报人应下载并阅读附件1-4，根据自身需求进行相关项目申报内容的了解，并按照每个文件内的附件中的申报书在2019年12月31日前登录教育部产学项目官方平台<a href="http://cxhz.hep.com.cn/index.html">“http://cxhz.hep.com.cn/index.html ”</a>进行注册申报；</p><br>
+              <p style="text-indent:2em;">2、项目申报人须在2020年6月30日前，将加盖学校公章的纸质申报书快递至南京云开数据科技有限公司，地址：江苏省南京市雨花台区凤台南路138号V泊赛创业大厦3楼305室，收件人：伍芳芳，电话：13357804061，并同时发送申报书电子文档至邮箱：980171045@qq.com；</p><br> 
+              <p style="text-indent:2em;">3、南京数据科技有限公司将于2020年6月底组织专家对申报项目进行遴选，并于2019年7月底-8月初公布立项名单；</p><br> 
+              <p style="text-indent:2em;">4、南京云开数据科技有限公司与项目负责人所在高校签署立项项目协议书，新工科建设、教学内容和课程体系改革、师资培训、实践条件和实践基地建设、创新创业教育改革项目要求在立项项目协议书签署后两年内完成；</p><br> 
+              <p style="text-indent:2em;">5、若有任何疑问，请致电：18268314670，联系人：黄山。</p><br> &nbsp;<br> 
+            </div><br>&nbsp;<br> 
+
+            <!-- 教育部快讯 -->
+            <div>
+              <h1>三、教育部快讯</h1><br>
+
+              <p style="text-indent:2em;"><a href="http://www.moe.gov.cn/s78/A08/A08_gggs/s8468/201911/t20191127_409903.html">快讯：教育部高等教育司关于公布有关企业支持的产学合作协同育人项目申报指南（2019年第二批）的通知(点击查看)</a></p><br> &nbsp;<br> 
+            </div><br>&nbsp;<br> 
+
+            <!-- 项目申报文件下载 -->
+            <div>
+              <h1>四、项目申报文件下载</h1><br>
+
+              <p style="text-indent:2em;">附件1：南京云开数据科技有限公司-教育部产学合作协同育人项目申报书（2019年第二批）<a href="../../../static/filezip/2019年第二批项目申报指南-南京云开数据科技有限公司.rar"> 点击下载</a></p><br>
+              <p style="text-indent:2em;">附件2：高校申报书模板（2019年第二批） <a href="../../../static/filezip/高校申报书模板.rar">点击下载 </a></p><br>
+              <p style="text-indent:2em;">附件3：合作协议模板（2019年第二批） <a href="../../../static/filezip/合作协议模板.rar">点击下载</a></p><br>
+              <p style="text-indent:2em;">附件4：项目结题报告模板（2019年第二批） <a href="../../../static/filezip/项目结题报告模板.rar">点击下载</a></p><br>
+            </div><br>&nbsp;<br> 
+
+            <!-- 申报回顾 -->
+            <div>
+              <h1>五、2018第二批、2019年第二批协同育人项目高校立项名单公示回顾：</h1><br>
+
+              <p style="text-indent:2em;">南京云开数据科技有限公司2018第二批协同育人项目高校立项名单 <a href="../../../static/filezip/2018年第二批协同育人项目立项名单.rar">点击下载</a></p><br>
+              <p style="text-indent:2em;">南京云开数据科技有限公司2019第一批协同育人项目高校立项名单 <a href="../../../static/filezip/2019年第一批协同育人项目立项名单.rar">点击下载</a></p><br>
+            </div><br>&nbsp;<br> 
+
           </div>
         </div>
       </div>
@@ -102,6 +140,10 @@ export default {
 
     a{
         color: red;
+    }
+
+    h1{
+      font-size: 1.4em;
     }
 
 
